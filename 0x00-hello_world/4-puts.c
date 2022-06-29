a61814f4d1a6df2c823b349395fcd58c2b9b1c3d
Iyvan-Kaba <kabaliivan@gmail.com>
2022-06-29 14:17:58 +0000

    Hello, puts_Task-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+/**
+ * main -> this is a function print main
+ * Return: success alwasy 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
