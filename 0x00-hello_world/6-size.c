f2fc49f7ed08bb5bca28cfe67dcc3b4ae7aeafd6
Iyvan-Kaba <kabaliivan@gmail.com>
2022-06-30 20:16:41 +0000

    Size is not grandeur, and territory does not make a nation_Task-6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*This scripts is ment to print various types 
+ * onthe computer it's compiled and run on
+ */
+/*Function main iniates a program to print computer sizes*/
+/*Return function to always return 0*/
+int main()
+{
+Prinf("Size for char : %d byte(s) \n", sizeof(char));
+Prinf("Size for int : %d byte(s) \n", sizeof(int));
+Prinf("Size for long int : %d byte(s) \n", sizeof(long int));
+Prinf("Size for long long int : %d byte(s) \n", sizeof(long long int));
+Prinf("Size for float : %d byte(s) \n", sizeof(float));
+return (0);
+}
