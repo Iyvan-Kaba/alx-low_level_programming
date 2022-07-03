bda97809dc22af7ee920fe7fa8edeb7affe168aa
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-03 08:52:25 +0000

    Patience, persistence and perspiration make an unbeatable combination for success_Task-9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,14 @@ int main(void)
 	int ch;
 	/** we utilise ASCII code interprated characters when using putchar*/
 	for (ch = 48; ch <= 57; ch++)
-		{
-		putchar(ch);
-		}
-
-	if (ch != 57)
+	{
+	putchar(ch);
+		if (ch != 57)
 		{
 		putchar(44);
 		putchar(32);
 		}
+	}
 	/**10 is the ASCII code for printing new line*/
 	putchar(10);
 return (0);
