1c15adabfe262567cf2eace6cdb4b2f4fcdee94a
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-03 09:08:43 +0000

    super-fix_Task-8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,21 +4,21 @@
  * followed by a new line.
  * Return: always 0 on success
  */
-int main(void);
+int main(void)
 {
-	int (n);
-	char (ch);
-for (n = 48; n <= 57; n++)
-{
-putchar(n);
-}
-for (ch = 'a'; ch <= 'f'; ch++)
-{
-putchar(ch);
-}
-
+	int ch;
+	char n;
+	/** we utilise ASCII code interprated characters when using putchar*/
+	for (ch = 48; ch <= 57; ch++)
+	{
+	putchar(ch);
+	}
+	for (n = 'a'; ch <= 'f'; n++)
+	{
+	putchar(n);
+	}
+	/**10 is the ASCII code for printing new line*/
 	putchar(10);
-
 return (0);
 }
 
