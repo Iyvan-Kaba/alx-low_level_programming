9dd046f5ac6c92620fefe272500e0518c81d90ba
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-06 01:06:37 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - adds two integers and returns the result.
+ *@int: is a parameter for the sum value of integers.
+ *Return - always returns value of 'add' x
+ */
+int add(int, int)
+{	
+	int a;
+	int b;
+	int x;
+
+	x = a + b;
+	return (x);
+}
