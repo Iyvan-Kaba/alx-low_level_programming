30367f327f56e2c69a99ee3cba42509eb9eb17ae
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-07 19:13:09 +0000

    fix

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+nclude "main.h"
+/**
+ *  * _isupper -> this is a function to check upper case alpahbets
+ *   * @c: integer parameter
+ *    * Return: always 0 on success
+ *     */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
