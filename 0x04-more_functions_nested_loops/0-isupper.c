9851211e126e89cb1803b41738c40166e8f1ace4
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-07 19:15:27 +0000

    hot fix it

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
-nclude "main.h"
+#include "main.h"
 /**
- *  * _isupper -> this is a function to check upper case alpahbets
- *   * @c: integer parameter
- *    * Return: always 0 on success
- *     */
+ *_isupper -> this is a function to check upper case alpahbets
+ *@c: integer parameter
+ *Return: always 0 on success
+ */
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
