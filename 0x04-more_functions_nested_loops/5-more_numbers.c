78f0ddf4001550d70c490c5060f8eb197da9a9af
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-08 11:29:17 +0000

    fixing task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,11 @@ void more_numbers(void)
 	for (x = 0; x < 10; x++)
 	{
 	for (y = 0; y < 15; y++)
-	if (y != 2 && y != 4)
 	{
-	_putchar(y + '0');
+	if (y >= 10)
+	_putchar((y / 10) + '0');
+	else
+	_putchar((y % 10) + '0');
 	}
 _putchar('\n');
 	}
