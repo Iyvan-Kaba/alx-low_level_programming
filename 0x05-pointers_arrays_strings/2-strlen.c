7158b6628df1768a72d3734ca8ad986c8b0bbf03
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-10 23:29:42 +0000

    Swap_lens-Task-2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strlen -> function to get the length of a string
+ *@s: string pointer passed to this function
+ *Return: returns lenght of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (*s != '\0'; s++)
+	{
+	len = len + 1;
+	}
+	return (strlen(s));
+}
