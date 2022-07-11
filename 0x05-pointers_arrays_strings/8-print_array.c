6df506d4e148633dd94c4641df4bf121feed88be
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-11 10:47:09 +0000

    Task-8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string>
+/**
+ *print_array -> prints n elements of an array of integers
+ *@a: string parameter1
+ *@n: string parameter2
+ */
+void print_array(int *a, int n)
+{
+
+}
