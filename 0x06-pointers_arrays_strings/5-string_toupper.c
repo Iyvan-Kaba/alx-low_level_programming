3b0e0d9f75030df9ff2f3e41102fe7de24e19717
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-14 17:50:18 +0000

    Task-5-Upper to Lower

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ *string_toupper -> converting of a string to uppercase
+ *@x: string param
+ *Return: string
+ */
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+	}
+	return (x);
+}
