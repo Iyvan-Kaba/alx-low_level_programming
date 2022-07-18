f682a42216fdac56a2fe658208162c390cc5368a
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-18 21:22:39 +0000

    task-4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strpbrk -> a function that searches a string
+ * @s: a string
+ * @accept: a character
+ * Return: a string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (_strpbrk(s, accept));
+}
