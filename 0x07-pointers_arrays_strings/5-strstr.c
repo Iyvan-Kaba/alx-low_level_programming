b20142c87cc7f030fdf2b2f2f07b3108b0438260
Iyvan-Kaba <kabaliivan@gmail.com>
2022-07-18 21:28:16 +0000

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strstr -> a function that locates a substring
+ * @haystack: a string
+ * @needle: a character
+ * Return: a string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (_strstr(haystack, needle));
+}
